6f46d1a47b7bea1049053a78e5e6d454c09c1a3f
Matheus-Tiago <145420370+Matheus-Tiago@users.noreply.github.com>
2024-07-16 14:27:47 -0300

    Aula03 AulaNodeJs
    Curso de Node JS Aula 03 Criar Rotas POST com Express, Como usar o Insomnia

--- aula03/API-REST/src/app.js
+++ aula03/API-REST/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const app = express()
 
+app.use(express.json())
+
 //mock
 const selecoes = [
     {id: 1, selecao: "Brasil", grupo: 'G' },
@@ -18,4 +20,8 @@ app.get('/selecoes', (req, res) =>{
     res.status(200).send(selecoes)
 })
 
+app.post('/selecoes', (req, res) =>{
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com sucesso!')
+})
 export default app
